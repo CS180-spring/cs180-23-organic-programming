a35f5aa71eda07c4cb295e616a64742c972b1ad9
adrian-resenedez <adrian-resendez@outlook.com>
2023-05-11 15:28:25 -0700

    filters. h and made some changes to main in adrianMain

---
+++ Backend/filters.h
@@ -0,0 +1,115 @@
+#ifndef FILTERS_H
+#define FILTERS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "Database.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/prettywriter.h"
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace std;
+
+class Filters
+{
+public:
+/*
+void outputFirstDataSet(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return;
+    }
+
+    rapidjson::IStreamWrapper isw(file);
+    rapidjson::Document doc;
+    doc.ParseStream(isw);
+
+    if (doc.HasParseError()) {
+        std::cerr << "Failed to parse JSON file: " << filename << std::endl;
+        return;
+    }
+
+    if (!doc.IsArray() || doc.Empty()) {
+        std::cerr << "JSON file does not contain an array or the array is empty" << std::endl;
+        return;
+    }
+
+    const rapidjson::Value& firstDataSet = doc[0];
+    // Output the first data set as a string
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    firstDataSet.Accept(writer);
+    std::cout << buffer.GetString() << std::endl;
+}*/
+std::vector<std::string> getKeysFromFirstObject(const std::string& filename) {
+    std::vector<std::string> keys;
+    std::ifstream file(filename);
+
+    if (file.is_open()) {
+        std::string jsonStr((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        rapidjson::Document doc;
+        doc.Parse(jsonStr.c_str());
+       // cout << jsonStr;
+        if (doc.IsArray() && !doc.Empty() && doc[0].IsObject()) {
+            for (auto it = doc[0].MemberBegin(); it != doc[0].MemberEnd(); ++it) {
+                keys.push_back(it->name.GetString());
+            }
+        }
+    }
+      else if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+    }
+    return keys;
+}
+
+void searchObject(string filename){
+        // Open the input stream and parse the JSON
+    std::ifstream input(filename);
+    IStreamWrapper isw(input);
+    Document document;
+    document.ParseStream(isw);
+
+    // Prompt the user to enter values for each key
+    std::string manga_name, volume_number, mangaka_name, publisher_name, print_edition, extra_comments;
+    std::cout << "Enter manga name: ";
+    std::getline(std::cin, manga_name);
+    std::cout << "Enter volume number: ";
+    std::getline(std::cin, volume_number);
+    std::cout << "Enter mangaka name: ";
+    std::getline(std::cin, mangaka_name);
+    std::cout << "Enter publisher name: ";
+    std::getline(std::cin, publisher_name);
+    std::cout << "Enter print edition: ";
+    std::getline(std::cin, print_edition);
+    std::cout << "Enter extra comments: ";
+    std::getline(std::cin, extra_comments);
+
+    // Iterate over the objects in the array
+    for (Value::ConstValueIterator itr = document.Begin(); itr != document.End(); ++itr) {
+        const Value& obj = *itr;
+        // Check if the object matches the user's input
+        if (obj["manga_name"].IsString() && obj["manga_name"].GetString() == manga_name &&
+            obj["volume_number"].IsString() && obj["volume_number"].GetString() == volume_number &&
+            obj["mangaka_name"].IsString() && obj["mangaka_name"].GetString() == mangaka_name &&
+            obj["publisher_name"].IsString() && obj["publisher_name"].GetString() == publisher_name &&
+            obj["print_edition"].IsString() && obj["print_edition"].GetString() == print_edition &&
+            obj["extra_comments"].IsString() && obj["extra_comments"].GetString() == extra_comments) {
+            // If it does, print the object
+            StringBuffer buffer;
+            Writer<StringBuffer> writer(buffer);
+            obj.Accept(writer);
+            std::cout << buffer.GetString() << std::endl;
+        }
+    }
+}
+
+};
+#endif
